a3919e5dfdf9e10ffbe6eea33bb2b64525f9c0dc
Allef Sousa <allefsousa_1@hotmail.com>
2017-12-04 01:09:25 -0200

    Adicionando ButterKnife

--- app/src/main/java/com/developer/allef/mynotes/principalActivity.java
+++ app/src/main/java/com/developer/allef/mynotes/principalActivity.java
@@ -1,13 +1,31 @@
 package com.developer.allef.mynotes;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class principalActivity extends AppCompatActivity {
+
+    @BindView(R.id.textnota)
+    TextInputEditText nota;
+    @BindView(R.id.texttitulo)
+    TextInputEditText titulo;
+    @BindView(R.id.textdata)
+    TextInputEditText data;
+    @BindView(R.id.btnsalvar)
+    Button salvar;
+    @BindView(R.id.btnvisualizar)
+    Button visualizar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        ButterKnife.bind(this);
     }
 }
